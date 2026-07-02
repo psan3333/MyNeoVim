5df07ad5021235568989c8abb71e855828350cfb
psan3333 <psan3333@mail.ru>
2026-07-02 12:41:37 +0700

    fix: remove annoying pyright notifiations on each keystroke (removed mini.notify)

--- init.lua
+++ init.lua
@@ -103,7 +103,7 @@ vim.opt.wildmode = "longest:full,full" -- complete longest common match, full co
 vim.opt.diffopt:append("linematch:60") -- improve diff display
 vim.opt.redrawtime = 10000 -- increase neovim redraw tolerance
 vim.opt.maxmempattern = 20000 -- increase max memory
-
+vim.opt.shell = "/usr/bin/zsh"
 -- ============================================================================
 -- STATUSLINE
 -- ============================================================================
@@ -219,37 +219,6 @@ _G.file_size = file_size
 vim.cmd([[
   highlight StatusLineBold gui=bold cterm=bold
 ]])
-
--- Function to change statusline based on window focus
---[[local function setup_dynamic_statusline()
-	vim.api.nvim_create_autocmd({ "WinEnter", "BufEnter" }, {
-		callback = function()
-			vim.opt_local.statusline = table.concat({
-				"  ",
-				"%#StatusLineBold#",
-				"%{v:lua.mode_icon()}",
-				"%#StatusLine#",
-				" > %f %h%m%r", -- nf-pl-left_hard_divider
-				"%{v:lua.git_branch()}",
-				"> ", -- nf-pl-left_hard_divider
-				"%{v:lua.file_type()}",
-				" > ", -- nf-pl-left_hard_divider
-				"%{v:lua.file_size()}",
-				"%=", -- Right-align everything after this
-				"> %l:%c  %P ", -- nf-fa-clock_o for line/col
-			})
-		end,
-	})
-	vim.api.nvim_set_hl(0, "StatusLineBold", { bold = true })
-
-	vim.api.nvim_create_autocmd({ "WinLeave", "BufLeave" }, {
-		callback = function()
-			vim.opt_local.statusline = "  %f %h%m%r \u{e0b1} %{v:lua.file_type()} %=  %l:%c   %P "
-		end,
-	})
-end
-
-setup_dynamic_statusline()]]
 
 -- ============================================================================
 -- KEYMAPS
@@ -625,7 +594,7 @@ local function setup_bufferline()
 				delay = 200,
 				reveal = { "close" },
 			},
-			sort_by = "insert_after_current",
+			sort_by = "id",
 		},
 	})
 end
@@ -665,7 +634,6 @@ require("mini.indentscope").setup({})
 require("mini.pairs").setup({})
 require("mini.trailspace").setup({})
 require("mini.bufremove").setup({})
-require("mini.notify").setup({})
 require("mini.icons").setup({})
 
 require("gitsigns").setup({
